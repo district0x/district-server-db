357951a3605e3a00aa57815a0b0f8d9bb1594d41
Juan Monetta <jpmonettas@gmail.com>
2018-06-07 09:56:50 -0300

    Now that honeysql needed PRs were merged we can bump up versions

--- project.clj
+++ project.clj
@@ -1,16 +1,12 @@
-(defproject district0x/district-server-db "1.0.1"
+(defproject district0x/district-server-db "1.0.2"
   :description "district0x server module for setting up database connection"
   :url "https://github.com/district0x/district-server-db"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[district0x/district-server-config "1.0.1"]
-                 ;[honeysql "0.9.1"]
-                 ;; Until PR https://github.com/jkk/honeysql/pull/195 is merged
-                 [madvas/honeysql "0.9.1"]
+                 [honeysql "0.9.2"]
                  [mount "0.1.11"]
-                 ;[nilenso/honeysql-postgres "0.2.3" :exclusions [honeysql]]
-                 ;; Until PR https://github.com/nilenso/honeysql-postgres/pull/13 is merged
-                 [madvas/honeysql-postgres "0.2.3" :exclusions [honeysql]]
+                 [nilenso/honeysql-postgres "0.2.4" :exclusions [honeysql]]
                  [org.clojure/clojurescript "1.9.946"]]
 
   :npm {:dependencies [[better-sqlite3 "4.0.3"]]
